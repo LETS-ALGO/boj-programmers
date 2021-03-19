0df934d86e74ece838928fdcf3d44702bf1770b7
SONG22 <ansongi527@gmail.com>
2021-03-19 14:05:20 +0900

    boj 11437.LCA 풀이 (DP)

--- SongI/src/graph/boj11437.java
+++ SongI/src/graph/boj11437.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 /*https://www.acmicpc.net/problem/11437
  * */
 public class boj11437 {
-
     static ArrayList<Integer>[] list;
     static int[][] parent;
     static int[] depth;
@@ -16,7 +15,7 @@ public class boj11437 {
         int n = sc.nextInt();
         list = new ArrayList[n + 1];
         for (int i = 0; i < n + 1; i++) list[i] = new ArrayList<>();
-        parent = new int[n + 1][17]; // 2^i > 50,000 인 i 는 16이다
+        parent = new int[n + 1][17]; // 2^i > 50,000 인 depth i 는 16이다
         depth = new int[n + 1];
 
         for (int i = 0; i < n - 1; i++) {
@@ -25,16 +24,22 @@ public class boj11437 {
             list[a].add(b);
             list[b].add(a);
         }
+        int tmp = 1;
+        int MAX_DEPTH = 0;
+        while (tmp <= n) {
+            tmp <<= 1;
+            MAX_DEPTH++;
+        } // 최대 depth 알아내는 식
+
         // 루트 노드 1에서 부터 전체 depth 계산
         dfs(1, 1);
 
-        for (int j = 1; j < 17; j++) {
+        for (int j = 1; j < MAX_DEPTH; j++) {
             for (int i = 1; i <= n; i++) {
                 //dp; i노드의 2^j번째 부모 계산
                 // j 가 2 일때 -> parent[i][2] = parent[parent[i][1]][1]
                 // i노드의 2^(j-1)번째 부모의 2^(j-1)번째 부모
-                // 즉 현재 노드의 바
-                parent[i][j] = parent[parent[i][j-1]][j-1];
+                parent[i][j] = parent[parent[i][j - 1]][j - 1];
             }
         }
         int m = sc.nextInt();
@@ -44,30 +49,36 @@ public class boj11437 {
             int b = sc.nextInt();
 
             // b의 depth가 더 깊도록
-            int answer = depth[a] > depth[b] ? lca(b, a) : lca(a, b);
+            int answer = depth[a] > depth[b] ? lca(b, a, MAX_DEPTH) : lca(a, b, MAX_DEPTH);
             System.out.println(answer);
         }
     }
 
-    public static void dfs(int start, int cnt) {
-        depth[start] = cnt;
+    public static void dfs(int start, int dep) {
+        depth[start] = dep;
         for (int next : list[start]) {
             if (depth[next] != 0) continue;
+            dfs(next, dep + 1);
             parent[next][0] = start; // 바로 위 (2^0)1번쨰 부모는 start
-            dfs(next, cnt += 1);
         }
     }
 
-    public static int lca(int a, int b) {
-        for(int i=16; i>=0; i--){
-            if(depth[b]-depth[a] >= (1<<i)){
+    public static int lca(int a, int b, int MAXD) {
+        // depth[a] < depth[b] 일 때
+        // 더 깊은 b 를 2승씩 점프하며 두 노드의 depth를 맞춘 후, 맞춘 depth의 조상 노드로 대체한다.
+        for (int i = MAXD - 1; i >= 0; i--) {
+            if (depth[b] - depth[a] >= (1 << i)) {
                 // i만큼 왼쪽 시프트 : 1* (2^i)
-                b = parent[b][i];
+                b = parent[b][i]; // b를 b의 2^i번째 조상 노드로 대체한다.
             }
         }
-        if(a == b) return a;
-        for(int i=16; i>=0; i--){
-            if(parent[a][i] != parent[b][i]){
+        // depth를 맞춘 후 대체한 조상노드가 a와 같으면 == b의 조상노드가 a라면 종료.
+        if (a == b) return a;
+
+        // 이제 두 노드는 같은 depth를 가지기 때문에
+        // 같이 2승씩 점프시키며 공통부모 바로 아래까지 올린다.
+        for (int i = MAXD - 1; i >= 0; i--) {
+            if (parent[a][i] != parent[b][i]) {
                 a = parent[a][i];
                 b = parent[b][i];
             }
